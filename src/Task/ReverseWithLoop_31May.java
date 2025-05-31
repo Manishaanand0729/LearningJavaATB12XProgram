70c714c424b0fb38bedcd80b088a0775abaf0f57
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-05-31 20:38:22 +0530

    init commit

---
+++ src/Task/ReverseWithLoop_31May.java
@@ -0,0 +1,16 @@
+package Task;
+
+public class ReverseWithLoop_31May {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Original String: " + str);
+        System.out.println("Reversed String: " + reversed);
+    }
+}
+
