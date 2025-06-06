4ba33185e37f11977925c0b639a5453c42281dea
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-06 22:05:16 +0530

    init commit

---
+++ src/Task/Find_Largest_Element_1stjune.java
@@ -0,0 +1,15 @@
+package Task;
+
+public class Find_Largest_Element_1stMay {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,10};
+        int max = arr [0];
+
+        for(int i = 0; i  < arr.length;i ++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println("Largest Num in Array: " + max);
+    }
+}
---
+++ src/Task/Smallest_num_1stJune.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class Smallest_num_1stMay {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int min = arr[0];
+         for(int i = 0; i > arr.length; i ++){
+             if(arr[i]> min){
+                 min = arr[i];
+             }
+         }
+        System.out.println("Smallest num In array: " + min);
+    }
+}
