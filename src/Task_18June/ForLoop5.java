b51ff4a6c6378756175fe75afab4911362ba89bc
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 22:16:05 +0530

    init commit

---
+++ src/Task_18June/ForLoop5.java
@@ -0,0 +1,10 @@
+package Task_18June;
+
+public class ForLoop5 {
+    public static void main(String[] args) {
+        int num = 5;
+        for(int i = 1; i <= 10; i ++){
+            System.out.println(num + " X " + i + " = " +(num * i));
+        }
+    }
+}
