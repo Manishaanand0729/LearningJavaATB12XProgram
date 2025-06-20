d4fb428e99bb663f3ce02f67c8165658009c6a6b
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 21:46:00 +0530

    init commit

---
+++ src/Task_17June/Voting_Eligibility.java
@@ -0,0 +1,12 @@
+package Task_17June;
+
+public class Voting_Eligibility {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18){
+            System.out.println("Eligibale to Vote");
+        }else {
+            System.out.println("Not Eligibale to Vote");
+        }
+    }
+}
