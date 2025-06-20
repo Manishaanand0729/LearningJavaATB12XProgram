8cf3de12203d15bbd1bb40ccb1955a96b9a78f96
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 22:36:55 +0530

    init commit

---
+++ src/Task_18June/WhileLoop5.java
@@ -0,0 +1,12 @@
+package Task_18June;
+
+public class WhileLoop5 {
+    public static void main(String[] args) {
+        int i = 1;
+        int num = 7;
+        while (i<= 10){
+            System.out.println(num + " X " + i + " = " + (num * i));
+            i ++;
+        }
+    }
+}
