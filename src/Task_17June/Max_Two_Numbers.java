1a073437c977e3a580ed53aecd82213e67b0612c
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 21:37:17 +0530

    init commit

---
+++ src/Task_17June/Max_Two_Numbers.java
@@ -0,0 +1,17 @@
+package Task_17June;
+
+public class Max_Two_Numbers {
+    public static void main(String[] args) {
+        int a = 14;
+        int b = 15;
+
+        if(a > b){
+            System.out.println("Maximum is " + a);
+        } else if (a < b) {
+            System.out.println(" Maximum is " + b);
+        }else{
+            System.out.println(" Both num is save");
+        }
+
+    }
+}
