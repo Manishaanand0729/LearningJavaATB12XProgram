26784d476f8f14a3560d7ddf8238e9bcf717b585
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 22:19:58 +0530

    init commit

---
+++ src/Task_18June/WhileLoop1.java
@@ -0,0 +1,11 @@
+package Task_18June;
+
+public class WhileLoop1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
