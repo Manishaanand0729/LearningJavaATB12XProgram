64cac833fb53ef550b7db2b5ec0196643bf81068
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 21:32:10 +0530

    init commit

---
+++ src/Task_17June/Check_evenOdd.java
@@ -0,0 +1,12 @@
+package Task_17June;
+
+public class Check_evenOdd {
+    public static void main(String[] args) {
+        int num = 2;
+        if(num % 2 == 0){
+            System.out.println(" This is a even");
+        }else {
+            System.out.println("This Is odd");
+        }
+    }
+}
