96697289364b9c5ee21887f977120dfe07d99bdf
Manisha Anand <manisha.anand0729@gmail.comgit config --global user.email manisha.anand0729@gmail.com>
2025-06-20 21:29:19 +0530

    init commit

---
+++ src/Task_17June/Check_Positive_Negative.java
@@ -0,0 +1,16 @@
+package Task_17June;
+
+public class Check_Positive_Negative {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num > 0) {
+            System.out.println("This Number Is Positive");
+        } else if (num<0) {
+            System.out.println("this Number is Negative");
+            
+        }else {
+            System.out.println("This number is Zero");
+        }
+
+    }
+}
